ad07312992805332b7123bd1d1f48ee496a106d1
Luis Vinicius <luisvpersy@gmail.com>
2022-12-04 00:20:36 -0300

    chore: creating repository

---
+++ src/repository/repository.ts
@@ -0,0 +1,17 @@
+import { FreelaDocument } from "../document/freela";
+import { UserDocument } from "../document/user";
+import User from "../models/user";
+export class Repository {
+
+  public findById = (_id: string) => User.findOne({_id: _id}).exec();
+
+  public register_user(user: UserDocument){
+    return new User(user).save()
+  }
+
+  public async register_freela(_id: string, freela: FreelaDocument){
+    const user = await User.findOne({_id})
+    user?.freelas.push(freela)
+    return user?.save()
+  }
+}
