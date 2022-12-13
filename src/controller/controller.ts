792e6a2430dd3f6dc647be31f77bf8be81e2faf8
Luis Vinicius <luisvpersy@gmail.com>
2022-12-13 18:32:13 -0300

    chore: create controller endpoint

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -2,9 +2,13 @@ import { Request, Response, Router } from 'express';
 import { UserDocument } from '../document/user';
 import { Service } from '../service/service';
 import { validateUserPayload } from './validations/register_user';
-import { toDocument } from './dto/user-request-dto';
+import { toUserDocument } from './dto/user-request-dto';
 import { InvalidPayloadException } from '../exceptions/invalid-payload-exception';
 import { UserFoundException } from '../exceptions/user_found';
+import { validateFreelaPayload } from './validations/register_freela';
+import { FreelaDocument } from '../document/freela';
+import { AmountOfLargeCharacters } from '../exceptions/amount-of-large-characters';
+import { toFreelaDocument } from './dto/freela-request-dto';
 
 export class Controller {
   private _router = Router();
@@ -16,13 +20,14 @@ export class Controller {
 
   constructor() {
     this._router.post('/', this.register_user);
+    this._router.post('/freela', this.register_freela);
   }
 
   private register_user = async (req: Request, res: Response) => {
     let user: UserDocument = req.body;
     try {
       validateUserPayload(user);
-      user = toDocument(user);
+      user = toUserDocument(user);
       await this.Service.createUser(user._id, user);
       return res.sendStatus(201);
     } catch (error) {
@@ -31,6 +36,26 @@ export class Controller {
       }
       if (error instanceof UserFoundException) {
         return res.status(302).send(error.message);
+      }
+    }
+  };
+
+  private register_freela = async (req: Request, res: Response) => {
+    const data = req.body;
+    let user_id: string = data.user_id;
+    delete data.user_id;
+    let freela: FreelaDocument = data;
+    try {
+      validateFreelaPayload(freela);
+      freela = toFreelaDocument(freela);
+      await this.Service.createFreela(user_id, freela);
+      return res.sendStatus(201);
+    } catch (error) {
+      if (error instanceof InvalidPayloadException) {
+        return res.status(422).send(error.message);
+      }
+      if (error instanceof AmountOfLargeCharacters) {
+        return res.status(413).send(error.message);
       }
     }
   };
